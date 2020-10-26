9c6cbf6449e2356b925173515bef43b42b26cf6c
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-10-26 15:27:29 -0700

    array methods

--- working_with_arrays/main.js
+++ working_with_arrays/main.js
@@ -52,10 +52,113 @@ for (const move of movements) {
   }
 }
 console.log('----------- forEach ------------')
-movements.forEach(move => {
+movements.forEach((move, i, arr) => {
   if(move > 0) {
-    console.log(`You deposited ${move}`);
+    console.log(`Move ${i+1}: You deposited ${move}`);
   } else {
-    console.log(`You withdrew ${Math.abs(move)}`);
+    console.log(`Move ${i+1}: You withdrew ${Math.abs(move)}`);
   }
 })
+
+/////////////////////////////////////
+// foreach with maps and sets
+console.log('-------- maps/sets---------');
+const currencies = new Map([
+  ['USD','US Dollar'],
+  ['EUR','Euro'],
+  ['GBP','Pound sterling'],
+]);
+
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
+
+// set
+const currencyUnique = new Set(['USD','GBP','USD','EUR','EUR']);
+currencyUnique.forEach((value, _, map) => {
+  console.log(`${value}`)
+})
+
+////////////////////////////////////////
+// more array methods
+
+const euroToUsd = 1.1;
+
+// map method
+// returns new array
+
+const moveUsd = movements.map(value => {
+  return value * euroToUsd;
+});
+
+console.log(movements);
+console.log(moveUsd);
+
+// filter method
+// filters array based on function
+// returns new array
+
+const deposits = movements.filter(value => {
+  return value > 0;
+});
+
+console.log(deposits);
+
+// reduce method
+// reduce values in array to one number
+// uses a parameter acc (accumulator) to reduce the array
+
+const balance = movements.reduce((acc, cur, i, arr) => {
+  return acc + cur;
+},0); // 0 is initial value of accumulator
+
+console.log(balance);
+
+// Chaining methods
+
+const total = movements
+  .filter(value => value > 0)
+  .map(value => value * euroToUsd)
+  .reduce((acc,value) => acc + value,0);
+
+console.log(total);
+
+// find method
+// finds and returns element passed in
+// returns the first element found that satisfies the condition
+
+const firstNeg = movements.find(value => value < 0);
+console.log(firstNeg);
+
+// Sorting array
+
+// ascending
+movements.sort((a,b) => {
+  if (a > b) {
+    return 1;
+  } 
+  if (a < b) {
+    return -1;
+  }
+});
+console.log(movements);
+
+// ascending cleaner
+movements.sort((a,b) => a - b);
+
+console.log(movements);
+
+//descending
+movements.sort((a,b) => {
+  if (a > b) {
+    return -1 
+  }
+  if (a < b) {
+    return 1
+  }
+})
+
+// descending cleaner
+movements.sort((a,b) => b - a);
+
+console.log(movements);
